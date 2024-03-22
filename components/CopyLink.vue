5fe965a7090fa028db101a104bc733bf79962a2d
Arkadiusz Zielinski <arkadiusz.zielinski@respect.energy>
2024-03-22 11:34:36 +0100

    3

--- components/CopyLink.vue
+++ components/CopyLink.vue
@@ -1,6 +1,6 @@
 <template>
 
-{{currentUrl}}
+<button @click="shareURL">Share URL</button>
 
     <div class="flex gap-4 justify-between">
         <button class="text-xs text-gray-600 dark:text-gray-50 opacity-50 pt-3" @click="copyToClipboard"><UIcon name="i-heroicons-document-duplicate" /><span> Skopiuj URL</span></button>
@@ -29,12 +29,35 @@ name: 'copyToClipboard',
   },
     methods: {
         copyToClipboard() {
-
             const textCopy = window.location.href;
             navigator.clipboard.writeText(textCopy)
             console.log(textCopy);
         },
+
+
+        async shareURL() {
+      if (navigator.share) {
+        try {
+          await navigator.share({
+            title: 'Your title',
+            text: 'Your description',
+            url: window.location.href
+          });
+          console.log('URL shared successfully');
+        } catch (error) {
+          console.error('Error sharing URL:', error);
+        }
+      } else {
+        console.log('Web Share API not supported');
+        // Fallback for browsers that do not support Web Share API
+        // You can implement your own sharing logic here
+      }
+    }
+
+
+
     },
+
 
 };
 </script>
